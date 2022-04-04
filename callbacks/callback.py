23f4f866c6f74c9bc840b64d4e5051620aec9adf
vad13irt <w13vadirt@gmail.com>
2022-04-04 19:43:58 +0300

    update early_stopping.py and callback.oy

--- callbacks/callback.py
+++ callbacks/callback.py
@@ -1,5 +1,6 @@
 from abc import abstractmethod, ABCMeta
 from dataclasses import dataclass, field
+import torch
 
 
 @dataclass
@@ -9,21 +10,55 @@ class Callback(metaclass=ABCMeta):
     ignore_warnings: bool = field(default=False)
     verbose: bool = field(default=False)
     
+
     def __post_init__(self):
         if self.mode not in ("min", "max"):
             raise ValueError(f"'mode' parameter shoud be 'min' or 'max', but given '{self.mode}'.")
         
-        assert (0 <= self.eps), f"'eps' parameter should be in range [0, +inf), but given '{self.eps}'." 
+        if not (0 <= self.eps):
+            raise ValueError(f"'eps' parameter should be in range [0, +inf), but given '{self.eps}'.")
+        
+        self._set_default_best_value()
     
+    
+    def state_dict(self) -> dict:
+        state = {
+            "eps": self.eps,
+            "mode": self.mode,
+        }
+        
+        return state
+        
+    def load_state_dict(self, state_dict):
+        self.eps = state_dict["eps"]
+        self.mode = state_dict["mode"]
+        
+        return self
     
     @abstractmethod
     def __call__(self):
         pass
     
-    def get_eps_value(self, value):
+    def _get_eps_value(self, value:float) -> float:
+        """
+        Gets 'value' in limits of 'eps' value relatively on 'mode'.
+        
+        """
         eps_value = (value - self.eps) if self.mode == "max" else (value + self.eps)
         return float(eps_value)
     
-    def is_better(self, value):
-        condition = self.best_value < value if self.mode == "max" else self.best_value > value      
+    def _is_better(self, value, best_value):
+        """
+        Compares 'value' and 'best_value' relatively on 'mode'. 
+        """
+        
+        condition = (best_value < value) if self.mode == "max" else (best_value > value)   
         return condition
+    
+    def _set_default_best_value(self):
+        """
+        Sets the minimum value for the 'best_value' relatively on 'mode'.
+        """
+        
+        best_value = torch.tensor(float("-inf" if self.mode == "max" else "inf"))
+        self.best_value = best_value
--- callbacks/early_stopping.py
+++ callbacks/early_stopping.py
@@ -1,42 +1,122 @@
+import torch
 from dataclasses import dataclass, field
-import numpy as np
+import logging
+import warnings
 from .callback import Callback
+
+
 
 
 @dataclass
 class EarlyStopping(Callback):
+    """
+    Inputs:
+        patience: int - number of attempts to beat 'best_value'. Default: 5.
+        best_value: float - the best value. Default: inf if 'mode' is 'min' otherwise -inf.
+        stopping_threshold: float - the threshold, when crossing it returns 'True'. Default: None.
+        check_gradient_vanishing: bool - checks for the problem Gradient Vanishing if True. Default: False.
+        logger: bool - logs messages if True. Default: True.
+    
+    """
+    
     patience: int = field(default=5)
-    past_value: float = field(default=np.inf)
+    best_value: float = field(default=float("inf"))
     stopping_threshold: float = field(default=None)
-    __fails: int = field(default=0)
+    check_gradient_vanishing: bool = field(default=False)
+    fails: int = field(default=0)
+    logger: bool = field(default=True)
     
-    def __post_init__(self):
-        assert 0 < self.patience, f"'patience' parameter should be in range (0, +inf), but given '{self.patience}'."
-        self.past_value = 0 if self.mode == "max" else np.inf
-    
-    
-    def is_worse_than_past(self, value):
-        condition = (self.past_value < value) if self.mode == "max" else (self.past_value > value)
-        return condition
-    
-    def check_threshold_condition(self, value):
-        condition = (self.stopping_threshold >= value) if self.mode == "max" else (self.stopping_threshold <= value)
-        return condition
-    
-    def __call__(self, value):
-        breaking = False
-        breaking = self.check_threshold_condition(value)
+    def __post_init__(self) -> None:
+        if not (0 < self.patience):
+            raise ValueError(f"'patience' parameter should be in range (0, +inf), but given '{self.patience}'.")
+         
+        if not isinstance(self.logger, logging.Logger):
+            if self.logger:
+                self.logger = logging.getLogger(__name__)
         
-        eps_value = self.get_eps_value(value)
-        condition = self.is_worse_than_past(eps_value)
+        self._set_default_best_value()
         
-        if not condition:
-            self.__fails += 1
-            if self.__fails >= self.patience:
-                breaking = True
-        else:
-            self.__fails = 0
+    
+    def state_dict(self) -> dict:
+        state = {
+            "patience": self.patience,
+            "best_value": self.best_value,
+            "check_gradient_vanishing": self.check_gradient_vanishing,
+            "stopping_threshold": self.stopping_threshold,
+            "fails": self.fails,
+            "eps": self.eps,
+            "mode": self.mode,
+            "is_stopped": self.is_stopped,
+        }
+        
+        if self.step is not None:
+            state["stopping_step"] = self.stopping_step
+        
+        return state
+    
+    def load_state_dict(self, state_dict):
+        self.patience = state_dict["patience"]
+        self.best_value = state_dict["best_value"]
+        self.check_gradient_vanishing = state_dict["check_gradient_vanishing"]
+        self.stopping_threshold = state_dict["stopping_threshold"]
+        self.fails = state_dict["fails"]
+        self.eps = state_dict["eps"]
+        self.mode = state_dict["mode"]
+        self.is_stopped = state_dict["is_stopped"]
+        
+        if "stopping_step" in state_dict:
+            self.stopping_step = state_dict["stopping_step"]
+        
+        return self
+    
+    def __call__(self, value:torch.Tensor, step=None) -> bool:
+        """
+        Inputs:
+            value: torch.Tensor - the value for controling Early Stopping.
+            step: int - step, where was checking for Early Stopping. If 'step' is provided, it will be displayed in the state.
             
-        self.past_value = value
+        Outputs:
+            stop: bool - controls stopping, if True the training process should be stopped.
+            
+        """
         
-        return breaking
+        if not isinstance(value, torch.Tensor):
+            raise "'value' must be torch.Tensor."
+        
+        eps_value = self._get_eps_value(value)
+        stop = False
+        if not torch.isfinite(value):
+            if self.check_gradient_vanishing:
+                stop = True
+                message = f"The value is not finite, Gradient Vanishing."
+            else:
+                if not self.ignore_warnings:
+                    warnings.warn(f"Detected Gradient Vanishing the training should be stopped.")
+        elif self.stopping_threshold is not None:
+            if self._is_better(best_value=eps_value, value=self.stopping_threshold):
+                stop = True
+                message = f"The value reaches the stopping threshold."
+            else:
+                message = f"The value doesn't reach the stopping threshold."
+        else:
+            if not self._is_better(best_value=eps_value, value=self.best_value):
+                improvement_delta =  abs(value - self.best_value)
+                message = f"'best_value' is improved by {improvement_delta}. New 'best_value': {value}."
+                self.best_value = value
+                self.fails = 0
+            else:
+                message = f"Attempt is failed to beat the 'best_value'."
+                self.fails += 1
+                if self.fails >= self.patience:
+                    stop = True
+                    message = f"Number of attempts is expired."
+        
+        if stop: 
+            self.stopping_step = step
+        
+        if self.logger:
+            self.logger.warning(message)
+            
+        self.is_stopped = stop
+        
+        return stop
