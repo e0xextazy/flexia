c22412e36754e2ecbbe6f55f6348173622117837
vad13irt <w13vadirt@gmail.com>
2022-04-04 19:52:08 +0300

    update trainer.py

--- callbacks/__init__.py
+++ callbacks/__init__.py
@@ -1,4 +1,4 @@
 from callback import Callback
-from callbacks import callbacks
+from callbacks import Callbacks
 from model_checkpoint import ModelCheckpoint
 from early_stopping import EarlyStopping
--- trainer.py
+++ trainer.py
@@ -1,12 +1,16 @@
 import torch
 from arguments import Arguments
 from utils import seed_everything, get_optimizer, get_scheduler
+from callbacks import Callbacks
 import random
 import warnings
+from torch.cuda.amp import GradScaler, autocast
+from contextlib import nullcontext
+import logging
 
 
-class Trainer:
-    def __init__(self, model, optimizer, criterion,  scheduler=None, model_parameters=None, arguments=Arguments(), logger=print):
+class Trainer:    
+    def __init__(self, model, optimizer, criterion, metric=None, scheduler=None, model_parameters=None, arguments=Arguments(), callbacks=Callbacks(), logger=logging.getLogger(__name__)):
         if isinstance(optimizer, dict):
             if model_parameters is None:
                 model_parameters = model.parameters()
@@ -18,144 +22,180 @@ class Trainer:
                 scheduler = get_scheduler(optimizer=optimizer, **scheduler)
 
         
-        if self.arguments.seed == "random":
-            self.arguments.seed = random.randint(a=0, b=1000)
-            warnings.warn("Setting the seed as 'random' maybe results in non-reproducible experiments.", category=warnings.WarningMessage)
+        if arguments.seed == "random":
+            arguments.seed = random.randint(a=0, b=1000)
+            if not arguments.ignore_warnings:
+                warnings.warn("Setting the seed as 'random' maybe results in non-reproducible experiments.")
+        elif arguments.seed == "none":
+            if not arguments.ignore_warnings:
+                warnings.warn("Setting the seed as 'none' maybe results in non-reproducible experiments, so you should care that you setted it before calling 'fit()' method.")
 
-        if self.arguments.save_scheduler_state and (scheduler is None):
-            if not self.arguments.ignore_warnings:
+        if arguments.save_scheduler_state and (scheduler is None):
+            if not arguments.ignore_warnings:
                 warnings.warn("The 'save_scheduler_state' parameter in Arguments was setted to the 'True', but the scheduler isn't setted, thereby  'save_scheduler_state' will be ignored.")
+        
+        if not isinstance(callbacks, Callbacks):
+            callbacks = Callbacks(callbacks)
             
         
-
+            
             
         self.model = model
         self.optimizer = optimizer
         self.criterion = criterion
+        self.metric = metric
         self.scheduler = scheduler
         self.arguments = arguments
-        self._criterion_name = self.criterion.__class__.__name__
-
-        seed = seed_everything(self.arguments.seed)
-        print(f"Seed: {seed}")
-
-
-
-    def __debug(self, message):
-        if self.arguments.debug:
-            print(message)
-
-
-    def collate_batch(self, batch, inputs_device="cpu", targets_device="cpu"):
-        if len(batch) == 2:
-            inputs, targets = batch
-            inputs = inputs.to(inputs_device)
-            targets = targets_device.to(targets_device)
-
-            return inputs, targets
-        else:
-            inputs = batch
-            inputs = inputs.to(inputs_device)
-
-            return inputs
-
-
-    def calculate_loss(self, outputs, targets):
-        loss = self.criterion(outputs, targets)
-        return loss
-
-
-    def save_checkpoint(self, path, loss=None, iteration=None, epoch=None):
-        checkpoint = {
-            "model_state": self.model.state_dict(),
-            "optimizer_state": self.optimizer.state_dict(),
-            "loss": loss,
-            "iteration": iteration,
-            "epoch": epoch,
-        }
-
-        if (self.scheduler is not None) and self.arguments.save_scheduler_state:
-            checkpoint["scheduler_state"] = self.scheduler.state_dict()
-
-        torch.save(checkpoint, path)
+        self.callbacks = callbacks
+        self.logger = logger
+        self.scaler = None
         
-        return checkpoint
-
-
-    def load_checkpoint(self, path):
-        checkpoint = torch.load(path)
-        model_state = checkpoint["model_state"]
-        self.model.load_state_dict(model_state)
-
-        optimizer_state = checkpoint["optimizer_state"]
-        self.optimizer.load_state_dict(optimizer_state)
-
-        if ("scheduler" in checkpoint) and (self.scheduler is not None):
-            scheduler_state = checkpoint["scheduler_state"]
-            self.scheduler.load_state_dict(scheduler_state)
-
-        return checkpoint
-
-
+        
+        if self.arguments.gradient_scaling:
+            if isinstance(self.arguments.gradient_scaling, dict):
+                self.scaler = GradScaler(**self.arguments.gradient_scaling)
+            else:
+                self.scaler = GradScaler()    
+                    
     def validate(self, loader):
-        loss = 0
+        loss, metrics = 0, None
         iterations = len(loader)
         for batch in loader:
             with torch.no_grad():
-                inputs, targets = batch
-                outputs = self.model(inputs)
-                batch_loss = self.calculate_loss(outputs, targets)
+                with self.autocast_context_manager():
+                    batch_loss, batch_outputs = self.calculate_loss(batch, return_outputs=True)
+                    batch_metrics = self.calculate_metrics(batch, batch_outputs)
+                    
                 loss += batch_loss.item()
-
+                
         loss /= iterations
         
-        return loss
-
-
-    def apply_gradient_clipping(self, parameters):
+        return loss, metrics
+    
+    def calculate_loss(self, batch, return_outputs=True):
+        inputs, targets = batch
+        outputs = self.model(inputs)
+        loss = self.criterion(outputs, targets)
+        
+        return (loss, outputs) if return_outputs else loss
+           
+    
+    def calculate_metrics(self, batch, outputs):
         pass
-
-
-    def train_one_batch(self, batch):
-        self.model.zero_grad()
+    
+    
+    def format_metrics(self, metrics, sep=" "):
+        string = ""
+        if metrics is not None:
+            string = sep.join([f"{k}: {v}" for k, v in metrics.items()])
+        
+        return string
+    
+    
+    def training_step(self, batch):
         self.model.train()
         
-        inputs, targets = self.collate_batch(batch, inputs_device=self.arguments.device, targets_device="cpu")
-        outputs = self.model(inputs)
-        batch_loss = self.calculate_loss(outputs, targets)
-
-
-        batch_loss.backward()
-        self.optimizer.step()
-
-        return batch_loss.detach()
-
+        with self.autocast_context_manager():
+            loss, outputs = self.calculate_loss(batch, return_outputs=True)
+            metrics = self.calculate_metrics(batch, outputs)
+        
+        loss /= self.arguments.gradient_accumulation_steps
+    
+        if self.arguments.gradient_scaling:
+            self.scaler.scale(loss).backward()
+        else:
+            loss.backward()
+            
+        
+        return loss.detach(), metrics
+    
+    def autocast_context_manager(self):
+        if self.arguments.amp:
+            context = autocast()
+        else:
+            context = nullcontext()
+            
+        return context
+    
 
     def fit(self, train_loader, validation_loader=None):
-        iterations_per_epoch = len(train_loader) 
-        iterations = int(iterations_per_epoch * self.arguments.epochs)
+        if self.arguments.seed is not None:
+            seed = seed_everything(self.arguments.seed)
+        
+        steps_per_epoch = len(train_loader) 
+        iterations = int(steps_per_epoch * self.arguments.epochs)
         passed_iterations = 1
-
+        
+        if self.arguments.debug:
+            self.logger.info(f"Epochs: {self.arguments.epochs}")
+            self.logger.info(f"Steps per epoch: {steps_per_epoch}")
+            self.logger.info(f"Gradient accumulation steps: {self.arguments.gradient_accumulation_steps}")
+            self.logger.info(f"Gradient norm: {self.arguments.gradient_norm}")
+            self.logger.info(f"Auto Mixed Precision: {self.arguments.amp}")
+            self.logger.info(f"Seed: {self.arguments.seed}")
+        
         if self.arguments.validation_strategy == "epoch":
-            self.arguments.validation_steps = iterations_per_epoch
+            self.arguments.validation_steps = steps_per_epoch
             if not self.arguments.ignore_warnings:
-                warnings.warn("When setting 'validation_strategy' equal to 'epoch', the 'validation_steps' parameter wiil be ignored.", category=warnings.WarningMessage)
-
-        self.model.train()
+                warnings.warn("When setting 'validation_strategy' equals to 'epoch', the 'validation_steps' parameter will be ignored.")
+        
+        if self.arguments.debug:
+            self.logger.info(f"Validation steps: {self.arguments.validation_steps}")
+        
+        print()
+        
+        self.logger.info("Start of training.")
+        self.model.zero_grad()
         self.model.to(self.arguments.device)
         for epoch in range(1, self.arguments.epochs+1):
             epoch_loss = 0
             for batch in train_loader:
-                train_loss = self.train_one_batch(batch)
+                train_loss, train_metrics = self.training_step(batch)
+                train_metrics_string = self.format_metrics(train_metrics)
                 
-                if (passed_iterations % self.arguments.validation_steps) == 0:
-                    validation_loss = self.validate(validation_loader)
-                    self.__debug(f"Iteration [{passed_iterations}/{iterations}] Validation Loss: {validation_loss}")
+                if passed_iterations % self.arguments.gradient_accumulation_steps == 0:
+                    if self.arguments.gradient_norm > 0:
+                        if self.arguments.gradient_scaling:
+                            self.scaler.unscale_(self.optimizer)
+                            
+                        torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=self.arguments.gradient_norm)
+                        
+                    if self.arguments.gradient_scaling:
+                        self.scaler.step(self.optimizer)
+                        self.scaler.update()
+                    else:
+                        self.optimizer.step()
+                        
+                    self.model.zero_grad()
+                
+                if self.arguments.verbose != "epoch":
+                    if (passed_iterations % self.arguments.verbose) == 0:
+                        self.logger.info(f"Epoch [{epoch}/{self.arguments.epochs}] Iteration [{passed_iterations}/{iterations}] Train Loss: {train_loss} {train_metrics_string}")
+                
+                if validation_loader is not None:
+                    if (passed_iterations % self.arguments.validation_steps) == 0:
+                        validation_loss, validation_metrics = self.validate(validation_loader)
+                        validation_metrics_string = self.format_metrics(validation_metrics)
+                        self.logger.info(f"Epoch [{epoch}/{self.arguments.epochs}] Iteration [{passed_iterations}/{iterations}] Validation Loss: {validation_loss} {validation_metrics_string}")
+                    
 
                 epoch_loss += train_loss
                 passed_iterations += 1
 
-            epoch_loss /= iterations_per_epoch
+            epoch_loss /= steps_per_epoch
 
+            self.logger.info(f"Epoch [{epoch}/{self.arguments.epochs}] Train Loss: {epoch_loss}")
+            print()
+        
+        if self.arguments.save_model:
+            torch.save(self.model.state_dict(), self.arguments.model_path)
+            self.logger.info(f"Model's weights saved to '{self.arguments.model_path}'.")
+    
+        
+        self.logger.info("End of training")
+        
+    def __str__(self):
+        return f"Trainer(arguments={str(self.arguments)})"
 
-        self.__debug(f"Epoch [{epoch}/{self.arguments.epochs}] Train Loss: {epoch_loss}")
+    
+    __repr__ = __str__
