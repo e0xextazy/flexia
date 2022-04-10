e52faf357e9b66371e09d4846362eb611f01daa3
vad13irt <w13vadirt@gmail.com>
2022-04-10 18:22:43 +0300

    add configuration.py

---
+++ torch_components/configuration.py
@@ -0,0 +1,58 @@
+import json
+from typing import Any
+from addict import Dict
+
+
+class Configuration:
+    """
+    Configuration provides you comfortable interface of saving your variables.
+    """
+    
+    def __init__(self, *args, **kwargs:dict) -> None:
+        if len(args) != 0:
+            raise ValueError(f"You must put attributes in format key=value.")
+            
+        self.attributes = Dict(kwargs)
+
+    def get_attributes_names(self) -> list:
+        return list(self.attributes.keys())
+                
+    def __getattr__(self, attribute:str) -> Any:
+        if attribute not in self.attributes:
+            attributes_names = self.get_attributes_names()
+            raise ValueError(f"Given attribute `{attribute}` is not setted in Configuration. Choose one of {attributes_names}.")
+        
+        return self.attributes[attribute]
+    
+    def __setitem__(self, attribute:str, value:Any) -> None:
+        self.attributes[attribute] = value
+    
+    def to_json_string(self) -> str:
+        return json.dumps(self.attributes)
+    
+    def to_json(self, path:str="configuration.json") -> str:
+        with open(path, "w", encoding="utf-8") as file:
+            data = self.to_json_string()
+            file.write(data)
+        
+        return path
+    
+    def from_json_string(self, string:str) -> dict:
+        return json.loads(string)
+    
+    def from_json(self, path:str="configuration.json") -> "Configuration":
+        with open(path, "r", encoding="utf-8") as file:
+            data = self.from_json_string(file.read())
+        
+        for k, v in data.items():
+            self.attributes[k] = v
+            
+        return self
+
+    
+    def __str__(self) -> str:
+        attributes_string = ", ".join([f"{k}={v}" for k, v in self.attributes.items()])
+        return f"Configuration({attributes_string})"
+    
+    __repr__ = __str__
+    __getitem__ = __getattr__
