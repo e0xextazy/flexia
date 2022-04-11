d4e94d1c6115e7f6dc6b2a99014be7f87bc1422b
vad13irt <w13vadirt@gmail.com>
2022-04-11 12:36:19 +0300

    added seed functions

--- torch_components/utils.py
+++ torch_components/utils.py
@@ -1,8 +1,35 @@
 import torch
+from torch import Optimizer
 import numpy as np
-import transformers as T
-from typing import Any
+from typing import Any, Union, Optional
+import random
+import os
 
+
+def get_random_seed(min_value:int=0, max_value:int=50) -> int:
+    return random.randint(min_value, max_value)
+
+def seed_everything(seed:Optional[int]=None) -> int:
+    if seed is None:
+        seed = get_random_seed()
+        
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+    os.environ['PC_SEED'] = str(seed)
+    
+    return seed
+    
+
+def get_lr(optimizer:Optimizer, only_last:bool=False) -> Union[int, list]:
+    if not isinstance(optimizer, Optimizer):
+        raise TypeError(f"The given `optimizer` type is not supported, it must be instance of Optimizer.")
+    
+    lrs = [param_group["lr"] for param_group in optimizer.param_groups]
+    return lrs[-1] if only_last else lrs
 
 
 def to_tensor(input:Any):
